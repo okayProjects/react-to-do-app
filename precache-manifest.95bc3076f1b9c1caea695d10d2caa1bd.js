e5fcbf6b68a08a07ff65c856bf0521aa87e1f680
unknown <lugowski.k@gmail.com>
2019-05-30 20:22:54 +0200

    Updates

---
+++ precache-manifest.95bc3076f1b9c1caea695d10d2caa1bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1e7f5d3ce0a3e86b46c",
+    "url": "/react-to-do-app/static/js/runtime~main.a1e7f5d3.js"
+  },
+  {
+    "revision": "03d53473ba900f17783c",
+    "url": "/react-to-do-app/static/js/main.03d53473.chunk.js"
+  },
+  {
+    "revision": "227790b2be4741aac0c1",
+    "url": "/react-to-do-app/static/js/2.227790b2.chunk.js"
+  },
+  {
+    "revision": "03d53473ba900f17783c",
+    "url": "/react-to-do-app/static/css/main.e75d3929.chunk.css"
+  },
+  {
+    "revision": "f4e8711a1c3b3b244f6c37309164011b",
+    "url": "/react-to-do-app/index.html"
+  }
+];
